33baaa87f1b1202b1640ea404e6c44e24d5c0b4f
Henrique Oliveira <henrique.oliverz@gmail.com>
2023-05-07 07:22:22 -0300

    Initiated hello page

---
+++ src/app/pages/hello/index.tsx
@@ -0,0 +1,14 @@
+import Logo from '@/components/logo'
+
+const Hello = () => (
+    <div>
+        <Logo />
+        <p>
+            Highly experienced developer with over 10 years in the industry.
+            Starting as a software developer, she quickly established herself as a skilled professional.
+            Today, as a software manager, her greatest strengths lie in her adaptability to changing environments and her exceptional leadership abilities.
+        </p>
+    </div>
+);
+
+export default Hello;
---
+++ src/components/logo.tsx
@@ -0,0 +1,7 @@
+const Logo = () => (
+    <h1 className="">
+        Henrique <p>Oliveira</p> <p>Software Engineering Manager</p>
+    </h1>
+);
+
+export default Logo;
