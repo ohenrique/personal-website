8bbecb2aba1aa1e8d52adf1463b8cbfe022c39d0
Henrique Oliveira <henrique.oliverz@gmail.com>
2023-05-10 08:59:20 -0300

    Adjust the carousel of portifolio page

--- src/app/pages/portfolio/works/index.tsx
+++ src/app/pages/portfolio/works/index.tsx
@@ -1,5 +1,10 @@
-import Carousel from 'react-elastic-carousel';
-import './index.css'
+import Image from 'next/image';
+
+import LeftArrow from '@public/left-arrow.png'
+import RightArrow from '@public/right-arrow.png'
+
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 interface Work {
     title: string
@@ -11,31 +16,41 @@ interface Work {
 const Works = (works: Work[]) => {
     return (
         <div>
-            {/* {WorksTimeline(works)} */}
-            <Carousel className='max-h-screen' renderPagination={() => (<></>)} renderArrow={() => (<></>)} pagination={false}>
-                {works.map(work => {console.log(work); return (
+            <Carousel className='max-h-screen'
+                emulateTouch={true}
+                showIndicators={false}
+                showThumbs={false}
+                showStatus={false}
+                preventMovementUntilSwipeScrollTolerance={true}
+                swipeScrollTolerance={50}
+                renderArrowPrev={(onClickHandler, hasPrev) =>
+                    hasPrev && (
+                        <a className='cursor-pointer z-50 absolute w-5 h-5 md:w-8 md:h-8 left-10 top-[90vh] md:top-[50vh] shadow-2xl' onClick={onClickHandler}>
+                            <Image color='white' src={LeftArrow.src} alt='' fill={true} />
+                        </a>
+                    )}
+                renderArrowNext={(onClickHandler, hasNext) =>
+                    hasNext && (
+                        <a className='cursor-pointer z-50 absolute w-5 h-5 md:w-8 md:h-8 right-10 top-[90vh] md:top-[50vh] shadow-2xl' onClick={onClickHandler}>
+                            <Image color='white' src={RightArrow.src} alt='' fill={true} />
+                        </a>
+                    )
+                }>
+                {works.map(work => (
                     <div key={crypto.randomUUID()}
-                        className={`min-h-screen bg-dark-gray bg-no-repeat bg-${work?.imageAlign || 'center'} bg-scroll bg-cover text-center
+                        className={`min-h-screen bg-dark-gray bg-no-repeat bg-${work?.imageAlign || 'center'} bg-scroll bg-cover text-center 
                         flex flex-wrap flex-col justify-center basis-full bg-blend-soft-light`}
-                        style={{ backgroundImage: `url(${work.image})` }} >
-                        <img className='inline-block mx-auto max-h-24 max-w-xs' src={work.icon} />
+                        style={{ backgroundImage: `url(${work.image})` }}>
+                        <Image className='inline-block mx-auto max-h-24 max-w-xs object-contain' src={work.icon} alt='' />
                         <h1 className='text-white text-center text-4xl md:text-5xl font-bold my-7'>{work.title}</h1>
                         <div>
                             <p className='inline-block text-white text-xl md:text-3xl font-thin md:max-w-lg px-2'>{work.description}</p>
                         </div>
                     </div>
-                )})}
+                ))}
             </Carousel>
         </div>
     )
 };
-
-const WorksTimeline = (works: Work[]) => (
-    <div>
-        {works.map(work => (WorkItem(work)))}
-    </div>
-)
-
-const WorkItem = (work: Work) => (<div>{work.icon}</div>)
 
 export default Works;
